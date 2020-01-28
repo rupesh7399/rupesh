07b98332707139a0d679c22838007e8b84d7824c
rupesh7399 <rupesh7399@gmail.com>
2020-01-28 18:59:31 +0530

    msg

--- Rupesh/Task/search_Module.py
+++ Rupesh/Task/search_Module.py
@@ -7,6 +7,9 @@ import re
 from html.parser import HTMLParser
 number_of_starttags = 0
 number_of_endtags = 0
+Urls = []
+resent = []
+
 
 class MyHTMLParser(HTMLParser):
     
@@ -18,7 +21,27 @@ class MyHTMLParser(HTMLParser):
         global number_of_endtags
         number_of_endtags += 1
 
+class Searchm(object):
 
+    def add_url(self, url):
+        self.url = url
+        URL = self.url
+        Urls.append(URL)
+        return Urls
 
+    def store(self):
+
+        return
     
+    def add_resent(self, res):
+        self.res = res
+        res = self.res
+        resent.append(res)
+        return resent
+
+    def search(self, key):
+        return
     
+    def check_Made(self, Key):
+        
+        return
